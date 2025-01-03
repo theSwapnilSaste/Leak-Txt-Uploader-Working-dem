3ad38ede4ebd2eb1f1a2a846515f25890d136cf5
Zenith-Official <zenithofficialhelp@gmail.com>
2025-01-04 01:07:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?template=https://github.com/Zenith-Officia/Leak-Txt-Uploader-Working.git)
---
+++ modules/config.py
@@ -0,0 +1,7 @@
+# config.py
+import os
+BOT_NAME = os.environ.get('BOT_NAME')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+API_ID = int(os.environ.get('API_ID'))
+API_HASH = os.environ.get('API_HASH')
+MONGO_URI = os.environ.get('MONGO_URI')
