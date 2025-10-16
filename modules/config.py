ee64fbd5a75591f4085769d06588eb88646d71ef
Swapnil Saste | INDIA <theswapnilsaste@gmail.Com>
2025-10-16 10:49:14 +0530

    new

--- modules/config.py
+++ modules/config.py
@@ -2,12 +2,13 @@
 import os
 
 # Bot Configuration
-BOT_NAME = os.environ.get('BOT_NAME', 'ZENITH_BOT')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '7626013382:AAEvmqpHw4otevswmJbtSw7yK03rGYVmc4M')
-API_ID = int(os.environ.get('API_ID', '22746239'))
-API_HASH = os.environ.get('API_HASH', 'a98ec8cfd8572a3a7c936cf828fe6215')
-MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://herukobanna:ankit999@cluster0.xs772me.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+BOT_NAME = os.environ.get('BOT_NAME', 'NANA_LTUW_BOT')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+API_ID = int(os.environ.get('API_ID', ''))
+API_HASH = os.environ.get('API_HASH', '')
+MONGO_URI = os.environ.get('MONGO_URI', '')
 
 # Additional Configuration
-OWNER_ID = int(os.environ.get('OWNER_ID', '7792539085'))
-LOG_CHANNEL_ID = int(os.environ.get('LOG_CHANNEL_ID', '7792539085'))
+OWNER_ID = int(os.environ.get('OWNER_ID', '504848353'))
+LOG_CHANNEL_ID = int(os.environ.get('LOG_CHANNEL_ID', '-1002971022913'))
+
